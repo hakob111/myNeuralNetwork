a03395ce98768e79db0e91899bbbf11ab2920c47
Hakob Chalabyan <chhakob6379@gmail.com>
2025-10-23 01:09:47 +0400

    Modification of query function comlite train function

--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -22,13 +22,28 @@ class MyNeuralNetwork:
             return weights
 
         self.we_in_to_hid = init_weights(self.hidnodes, self.inodes, self.hidnodes)
-        self.we_hid_to_out = torch.rand(self.outnodes, self.hidnodes, self.outnodes)
-        pass
+        self.we_hid_to_out = init_weights(self.outnodes, self.hidnodes, self.outnodes)
 
-    def train(self):
-        pass
+    def train(self, inputs_list, targets_list):
+        """This is a train function for training model
+            Here the error is back-propagated through the weight values.
+            using differential equations """
+        inputs = torch.tensor(inputs_list, dtype=torch.float32).unsqueeze(0).T
+        targets = torch.tensor(targets_list, dtype=torch.float32).unsqueeze(0).T
+        hidden_inputs = torch.matmul(self.we_in_to_hid, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = torch.matmul(self.we_hid_to_out, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        output_errors = targets - final_outputs
+        hidden_errors = torch.matmul(self.we_hid_to_out.T, output_errors)
+        # sigmoid * (1 -sigmoid) error propagation gradient
+        self.we_hid_to_out += self.lr * torch.matmul((output_errors * final_outputs * (1.0 - final_outputs)),
+                                                    torch.transpose(hidden_outputs))
+        self.we_in_to_hid += self.lr * torch.matmul((hidden_errors * hidden_outputs * (1 - hidden_outputs)),
+                                                    torch.transpose(inputs))
 
     def query(self, input_list):
+        """ it is a prediction function """
         inputs = torch.tensor(input_list, dtype=torch.float32).reshape(-1, 1)
         hidden_inputs = torch.matmul(self.we_in_to_hid, inputs)
         hidden_outputs = self.activation_function(hidden_inputs)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from MyNeuralNetwork import MyNeuralNetwork
 
 n = MyNeuralNetwork(3, 3, 3, 0.3)
+a = n.query([1.0, 0.5, -1.5])
 
+print(a)
